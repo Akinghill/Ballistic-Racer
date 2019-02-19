3a62f0623af8b198bcc7967917044b348f7cdc34
stlrnate <47372789+stlrnate@users.noreply.github.com>
2019-02-19 18:23:56 -0500

    oops forgot this online

--- Assets/Network/Scripts/PlayerUnit.cs
+++ Assets/Network/Scripts/PlayerUnit.cs
@@ -37,7 +37,7 @@ public class PlayerUnit : NetworkBehaviour {
 		}
 
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * -90.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 15.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 150.0f;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
