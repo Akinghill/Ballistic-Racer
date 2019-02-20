2767a5dc788383376ca905006976dde7ac5f77dd
adavidson617 <44651006+adavidson617@users.noreply.github.com>
2019-02-19 23:28:07 -0500

    Added even more things

---
+++ Assets/Scripts/TestLevels.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestLevels : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            PlayMovingPlatforms();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            PlayExplosion();
+        }
+    }
+
+    public void PlayMovingPlatforms()
+    {
+        SceneManager.LoadScene("Rose_MichaelB");
+    }
+
+    public void PlayExplosion()
+    {
+        SceneManager.LoadScene("JamieBTestExpPart");
+    }
+}
