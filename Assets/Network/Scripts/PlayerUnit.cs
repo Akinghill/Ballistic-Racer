7c84fcc8bd72adb20061aea7b61c3c6ae6e09dc9
stlrnate <47372789+stlrnate@users.noreply.github.com>
2019-02-26 19:21:15 -0500

    controller in online

--- Assets/Network/Scripts/PlayerUnit.cs
+++ Assets/Network/Scripts/PlayerUnit.cs
@@ -36,8 +36,8 @@ public class PlayerUnit : NetworkBehaviour {
 			Destroy(gameObject);
 		}
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * -90.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 150.0f;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * -120.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 75.0f;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
