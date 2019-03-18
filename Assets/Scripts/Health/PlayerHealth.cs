d9dfc818361e23b6bdd79d26dc2e4d6a69a40a65
Chronarc <mboggs185@gmail.com>
2019-03-18 17:31:07 -0400

    Particle effect fixes!

--- Assets/Scripts/Health/PlayerHealth.cs
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -70,7 +70,7 @@ public class PlayerHealth : MonoBehaviour
             currentHealth = startingHealth;
         }
 
-        if(currentHealth > 50)
+        if(currentHealth < 50)
         {
             SparkFX.GetComponent<ParticleSystem>().Play();
         }
