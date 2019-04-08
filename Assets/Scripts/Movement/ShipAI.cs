dd0cc4207a3dcd3f1632c41c5537f1841d640b8f
adavidson617 <44651006+adavidson617@users.noreply.github.com>
2019-04-08 15:35:00 -0400

    Small AI fix

--- Assets/Scripts/Movement/ShipAI.cs
+++ Assets/Scripts/Movement/ShipAI.cs
@@ -8,8 +8,6 @@ public class ShipAI : MonoBehaviour
     [Range(0, 1)]
     public float acceleration;
 
-    //AIPath path;
-    //List<Transform> nodes = new List<Transform>();
     Transform[] nodes;
     public int currentNode = 0;
 
@@ -23,8 +21,6 @@ public class ShipAI : MonoBehaviour
     ShipMovement shipMovement;
 
     PlayerShooting shooting;
-
-    //PlayerHealth playerHealth;
 
     [HideInInspector] public GameObject pathOne;
     [HideInInspector] public Transform[] m_pathOne;
@@ -40,21 +36,9 @@ public class ShipAI : MonoBehaviour
     void Start()
     {
         shootableMask = LayerMask.GetMask("Shootable");
-        //path = FindObjectOfType<AIPath>();
         input = GetComponent<PlayerInput>();
         shooting = GetComponentInChildren<PlayerShooting>();
-        //playerHealth = GetComponentInChildren<PlayerHealth>();
         shipMovement = GetComponent<ShipMovement>();
-
-        //Transform[] pathTransforms = path.GetComponentsInChildren<Transform>();
-        //nodes = new List<Transform>();
-        //for (int i = 0; i < pathTransforms.Length; i++)
-        //{
-        //    if (pathTransforms[i] != path.transform)
-        //    {
-        //        nodes.Add(pathTransforms[i]);
-        //    }
-        //}
 
         pathOne = GameObject.FindGameObjectWithTag("AI Path 1");
         int numOfNodesOnPathOne = pathOne.transform.childCount;
@@ -132,28 +116,10 @@ public class ShipAI : MonoBehaviour
 
     void Rudder()
     {
-        //if (playerHealth.currentHealth < 75)
-        //{
-        //    if (randomPath <= 5)
-        //    {
-        //        nodes = m_pathTwo;
-        //    }
-        //    else
-        //    {
-        //        nodes = m_pathThree;
-        //    }
-        //}
-        //else
-        //{
-        //    nodes = m_pathOne;
-        //}
-
         //Vector3 relativeVector = transform.InverseTransformPoint(nodes[currentNode].position);
-
         //float directionX;
         //float directionY;
         //float directionZ;
-
         //directionX = relativeVector.x / relativeVector.magnitude;
         //directionY = relativeVector.y / relativeVector.magnitude;
         //directionZ = relativeVector.z / relativeVector.magnitude;
@@ -161,41 +127,20 @@ public class ShipAI : MonoBehaviour
         Vector3 nextNodeDirection = nodes[currentNode].position - transform.position;
 
         float angle = Vector3.SignedAngle(transform.forward, nextNodeDirection, shipMovement.normal);
-        //Debug.Log(angle);
 
         if (angle < -5.0f)
         {
-            if (angle < -40.0f)
-            {
-                input.brake = 1.0f;
-                //input.rudder = directionX;
-                input.rudder = 0.0f;
-                input.brake = 0.0f;
-            }
-            else
-            {
-                input.brake = 1.0f;
-                //input.rudder = -directionZ;
-                input.rudder = -1.0f;
-                input.brake = 0.0f;
-            }
+            input.brake = 1.0f;
+            //input.rudder = -directionZ;
+            input.rudder = -1.0f;
+            input.brake = 0.0f;
         }
         else if (angle > 5.0f)
         {
-            if (angle > 40.0f)
-            {
-                input.brake = 1.0f;
-                //input.rudder = directionX;
-                input.rudder = 0.0f;
-                input.brake = 0.0f;
-            }
-            else
-            {
-                input.brake = 1.0f;
-                //input.rudder = directionZ;
-                input.rudder = 1.0f;
-                input.brake = 0.0f;
-            }
+            input.brake = 1.0f;
+            //input.rudder = directionZ;
+            input.rudder = 1.0f;
+            input.brake = 0.0f;
         }
         else
         {
