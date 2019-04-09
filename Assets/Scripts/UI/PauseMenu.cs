89ca7ce3dd671f274f998e1760ca11d35cb2b7b1
adavidson617 <44651006+adavidson617@users.noreply.github.com>
2019-04-09 14:49:08 -0400

    Fix for spawning sideways, for pausing when trying to boost, and for menu navigation being inverted

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -24,7 +24,7 @@ public class PauseMenu : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButtonDown("Submit"))
+        if (Input.GetButtonDown("Pause"))
         {
             if (GameIsPaused)
             {
