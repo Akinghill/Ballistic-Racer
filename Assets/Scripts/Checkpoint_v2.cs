8dcb5d1ecec42ab76863df38fb721a20d70ec56e
strongr_togethr <hodgejamie77@gmail.com>
2019-04-09 20:09:06 -0400

    Checkpoints stuff
    Checkpoints added to tracks 1 and 2 as well as new UI showing

--- Assets/Scripts/Checkpoint_v2.cs
+++ Assets/Scripts/Checkpoint_v2.cs
@@ -7,14 +7,44 @@ public class Checkpoint_v2 : MonoBehaviour {
     static Transform playerTransform;
     public Text checkpointNum;
     public Text lapNum;
+    public Text timerText;
+    float startTime;
+    float endTime;
+    float levelTimer;
+    bool updateTimer;
+    bool alapCompleted = false;
+    string minutes;
+    string seconds;
+    float t;
+    
     void Start()
     {
+
+        startTime = Time.timeSinceLevelLoad;
+        
         playerTransform = GameObject.FindGameObjectWithTag("Crash").transform;
         if (GameObject.FindGameObjectWithTag("Crash") != null)
         {
             Debug.Log("It has been found.");
         }
+        Visual();
         
+
+    }
+    void Update()
+    {
+      
+        float t = Time.timeSinceLevelLoad - startTime;
+        string minutes = Mathf.Floor((int)t / 60).ToString("00");
+        string seconds = Mathf.Floor(t % 60).ToString("00");
+        timerText.text = "Time Elapsed:" + minutes + ":" + seconds;
+ 
+        if(updateTimer)
+        {
+            levelTimer += startTime;
+        }
+
+
 
     }
 
@@ -46,7 +76,9 @@ public class Checkpoint_v2 : MonoBehaviour {
             {
                 //If we dont have any Checkpoints left, go back to 0
                 Laps.currentCheckpoint = 0;
+                alapCompleted = true;
             }
+            Visual();
             checkpointNum.text = "Checkpoints:" + Laps.currentCheckpoint;
             lapNum.text = "Lap Number:" + Laps.currentLap;
         }
@@ -55,7 +87,23 @@ public class Checkpoint_v2 : MonoBehaviour {
     }
     void Visual ()
     {
-       
+        foreach (Transform checkpoint in playerTransform.GetComponent<Laps>().checkPointArray)
+        {
+
+            Color color = checkpoint.GetComponent<Renderer>().material.color;
+            color.a = 0.2f;
+            checkpoint.GetComponent<Renderer>().material.color = color;
+
+
+
+
+        }
+        Color otherColor = playerTransform.GetComponent<Laps>().checkPointArray[Laps.currentCheckpoint].GetComponent<Renderer>().material.color;
+        otherColor.a = 0.8f;
+        playerTransform.GetComponent<Laps>().checkPointArray[Laps.currentCheckpoint].GetComponent<Renderer>().material.color = otherColor;
+
+
+
     }
 
 
