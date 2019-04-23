8d89af611e886aee073a63d60f21fcc5e4a4003b
stlrnate <47372789+stlrnate@users.noreply.github.com>
2019-04-23 19:35:51 -0400

    health script

---
+++ Assets/Scripts/Health/HealthRing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthRing : MonoBehaviour
+{
+    public int healthAmmount;
+
+    public float timeToRestart;
+
+    bool deactivated;
+
+    IEnumerator ActivationTime()
+    {
+        deactivated = true;
+        yield return new WaitForSeconds(timeToRestart);
+        deactivated = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.parent.tag == "Ship")
+        {
+            if (!deactivated)
+            {
+                other.GetComponentInParent<PlayerHealth>().currentHealth += healthAmmount;
+                StartCoroutine(ActivationTime());
+            }
+        }
+    }
+}
