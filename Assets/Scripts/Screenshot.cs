ddc45b3da904a2788d622f14e80ce8090c3fabe9
adavidson617 <44651006+adavidson617@users.noreply.github.com>
2019-05-09 13:28:07 -0400

    Added ability to take screenshots in game
    Press F3 to take a screenshot. Screenshots save to the persistent data directory

---
+++ Assets/Scripts/Screenshot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+    // The folder to contain our screenshots.
+    // If the folder exists we will append numbers to create an empty folder.
+    public string folder = "ScreenshotFolder";
+
+    void Start()
+    {
+        // Create the folder
+        System.IO.Directory.CreateDirectory(Application.persistentDataPath + "/" + folder);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            DateTime dateTime = DateTime.Now;
+            string date = dateTime.ToString("yyyy-MM-dd HH.mm.ss");
+
+            string name = string.Format(Application.persistentDataPath + "/{0}/Screenshot {1}.png", folder, date);
+
+            // Capture the screenshot to the specified file.
+            ScreenCapture.CaptureScreenshot(name);
+        }
+    }
+}
